7a92e357312f15f74ed48d2a1547969dc1c673dd
tnei <tylerneil1@hotmail.com>
2024-09-13 16:39:09 -0400

    Update snowball_app.py

--- snowball_app.py
+++ snowball_app.py
@@ -96,6 +96,9 @@ data = load_dummy_data()
 st.sidebar.header("Filter Options")
 start_month = st.sidebar.date_input("Start Date", value=data['date'].min())
 end_month = st.sidebar.date_input("End Date", value=data['date'].max())
+product_selection = st.sidebar.multiselect("Select products:", ['Design Live', 'Winner Flex', 'Insight'], default=['Design Live', 'Winner Flex'])
+forecast_period = st.sidebar.slider("Forecast period (months):", 1, 12, value=6)
+contract_length_range = st.sidebar.slider("Contract length (months):", 2, 48, value=(2, 48))
 
 # Convert input to pandas datetime if necessary (in case the input is not a datetime object)
 if isinstance(start_month, pd.Timestamp):
